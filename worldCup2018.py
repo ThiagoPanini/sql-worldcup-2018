13e509ac9ee8145fbd4dd418af02eeab799a271e
thiagoPanini <thipanini94@gmail.com>
2018-05-22 23:56:46 -0300

    Initial commit

---
+++ worldCup2018.py
@@ -0,0 +1,36 @@
+"""Projeto Copa do Mundo 2018 - Análise"""
+
+
+class CorPy:
+    azul = '\033[1;34m'
+    vermelho = '\033[1;31m'
+    turquesa = '\033[1;36m'
+    amarelo = '\033[1;33m'
+    verde = '\033[1;32m'
+    end = '\033[m'
+
+
+class selecao:
+    brasil = {'Nome': 'Brasil', 'J': 0, 'P': 0, 'V': 0, 'GP': 0, 'GC': 0, 'SG': 0,
+              'Jogadores': {1: 'Alisson', 22: 'Fagner', 3: 'Miranda', 4: 'Thiago Silva', 12: 'Marcelo',
+                            5: 'Casemiro', 8: 'Renato Augusto', 15: 'Paulinho',
+                            10: 'Neymar', 11: 'Coutinho', 9: 'G. Jesus'}}
+
+    suica = {'Nome': 'Suiça', 'J': 0, 'P': 0, 'V': 0, 'GP': 0, 'GC': 0, 'SG': 0,
+             'Jogadores': {1: 'Sommer', 2: 'Lichtsteiner', 22: 'Schär', 12: 'Akanji', 13: 'R. Rodriguez',
+                           5: 'Zakaria', 10: 'Xhaka', 15: 'Dzemaili',
+                           23: 'Shaqiri', 14: 'Zuber', 9: 'Seferovic'}}
+
+    servia = {'Nome': 'Sérvia', 'J': 0, 'P': 0, 'V': 0, 'GP': 0, 'GC': 0, 'SG': 0,
+              'Jogadores': {1: 'Stojkovic', 2: 'Rukavina', 22: 'Tosic', 12: 'Ivanovic', 13: 'Kolarov',
+                           21: 'Matic', 10: 'M. Savic', 15: 'Gudelj',
+                           23: 'Ljajic', 14: 'Tadic', 9: 'Mitrovic'}}
+
+    costaRica = {'Nome': 'Costa Rica', 'J': 0, 'P': 0, 'V': 0, 'GP': 0, 'GC': 0, 'SG': 0,
+                 'Jogadores': {1: 'Keylor Navas', 2: 'Waston', 22: 'Acosta', 12: 'Gonzales', 13: 'Oviedo',
+                               21: 'Gamboa', 10: 'Guzman', 15: 'Borges',
+                               23: 'C. Bolaños', 14: 'Bryan Ruiz', 9: 'Ureña'}}
+
+
+
+
