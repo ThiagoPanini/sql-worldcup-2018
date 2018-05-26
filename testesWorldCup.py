afa157bd9a78a64bc08e36671e55c2feac32f314
thiagoPanini <thipanini94@gmail.com>
2018-05-26 13:50:05 -0300

    Aprimoramento do código

--- testesWorldCup.py
+++ testesWorldCup.py
@@ -5,16 +5,32 @@ decidir um campeão baseado em um único critério: favorito ou zebra."""
 
 from time import sleep
 from random import choice
+from itertools import combinations
 
 fav = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]  # Quantidade de gols por partida a serem sorteados para o time Favorito
 zebra = [0, 0, 0, 0, 1, 1, 1, 1, 2, 3]  # Quantidade de gols por partida a serem sorteados para times não-Favoritos
+nomesGrupos = ['Grupo A', 'Grupo B', 'Grupo C', 'Grupo D', 'Grupo E', 'Grupo F', 'Grupo G', 'Grupo H']
+
+
+def cabecalhoRodada(n):
+    """Imprime cabeçalho indicativo do número da RODADA"""
+    print('-' * 35)
+    print('{:^35}'.format('RODADA ' + str(n)))
+    print('-' * 35)
+
+
+def cabecalhoGrupo(texto):
+    """Imprime cabeçalho indicativo do número da RODADA"""
+    print('-' * 35)
+    print('{:^35}'.format(texto))
+    print('-' * 35)
 
 
 def placar(time1, placar1, time2, placar2):
     """Imprime placar baseado no sorteio feito nas listas"""
-    print('-' * 35)
+    # print('-' * 35)
     print('{:>12}{:>4} x {:<4}{}'.format(time1, placar1, placar2, time2))
-    print('-' * 35)
+    # print('-' * 35)
 
 
 class Selecao():
@@ -47,7 +63,7 @@ class Selecao():
         casa.status['SG'] = casa.status['GP'] - casa.status['GC']  # Atualiza "Saldo de Gols" do time da Casa
         visitante.status['J'] += 1  # Atualiza a quantidade de "Jogos" do time Visitante
         visitante.status['GP'] += golsVisit  # Atualiza a quantiade de "Gols Pro" do time Visitante
-        visitante.status['GC'] += golsVisit  # Atualiza a quantidade de "Gols Contra" do time Visitante
+        visitante.status['GC'] += golsHome  # Atualiza a quantidade de "Gols Contra" do time Visitante
         visitante.status['SG'] = visitante.status['GP'] - visitante.status['GC']  # "Saldo de Gols" do Visitante
 
         if golsHome > golsVisit:  # Caso o time da "Casa" vença, atualiza "Vitórias" e "Pontos" (+3)
@@ -62,8 +78,13 @@ class Selecao():
             visitante.status['V'] += 1
             visitante.status['P'] += 3
 
-    def imprimeTabela(self):
-        pass
+    def organizaConfronto(self, grupos):
+        for grupo in grupos:
+            grupo = list(combinations(grupo, 2))
+
+    def imprimeTabela(self, grupo):
+        print('{}{}{}')
+
 
 
 """Instanciando todas as seleções"""
@@ -74,15 +95,15 @@ egito = Selecao('Egito', True)
 russia = Selecao('Russia', False)
 uruguai = Selecao('Uruguai', True)
 
-grupoA = [arabia, egito, russia, uruguai]
+grupoA = [arabia, egito, russia, uruguai]  # Objetos (seleções) do Grupo A guardados na lista grupoA
 
 # Grupo B
 espanha = Selecao('Espanha', True)
 ira = Selecao('Irã', False)
 marrocos = Selecao('Marrocos', False)
-portugual = Selecao('Portugal', True)
+portugal = Selecao('Portugal', True)
 
-grupoB = [espanha, ira, marrocos, portugual]
+grupoB = [espanha, ira, marrocos, portugal]  # Objetos (seleções) do Grupo B guardados na lista grupoB
 
 # Grupo C
 australia = Selecao('Austrália', False)
@@ -90,7 +111,7 @@ dinamarca = Selecao('Dinamarca', False)
 franca = Selecao('França', True)
 peru = Selecao('Peru', False)
 
-grupoC = [australia, dinamarca, franca, peru]
+grupoC = [australia, dinamarca, franca, peru]  # Objetos (seleções) do Grupo C guardados na lista grupoC
 
 # Grupo D
 argentina = Selecao('Argentina', True)
@@ -98,7 +119,7 @@ croacia = Selecao('Croácia', False)
 islandia = Selecao('Islândia', False)
 nigeria = Selecao('Nigéria', False)
 
-grupoD = [argentina, croacia, islandia, nigeria]
+grupoD = [argentina, croacia, islandia, nigeria]  # Objetos (seleções) do Grupo D guardados na lista grupoD
 
 # Grupo E
 brasil = Selecao('Brasil', True)
@@ -106,7 +127,7 @@ suica = Selecao('Suiça', False)
 costa = Selecao('Costa Rica', False)
 servia = Selecao('Sérvia', False)
 
-grupoE = [brasil, suica, costa, servia]
+grupoE = [brasil, suica, costa, servia]  # Objetos (seleções) do Grupo E guardados na lista grupoE
 
 # Grupo F
 alemanha = Selecao('Alemanha', True)
@@ -114,7 +135,7 @@ coreia = Selecao('Coreia', False)
 mexico = Selecao('México', False)
 suecia = Selecao('Suécia', False)
 
-grupoF = [alemanha, coreia, mexico, suecia]
+grupoF = [alemanha, coreia, mexico, suecia]  # Objetos (seleções) do Grupo F guardados na lista grupoF
 
 # Grupo G
 belgica = Selecao('Bélgica', True)
@@ -122,7 +143,7 @@ inglaterra = Selecao('Inglaterra', True)
 panama = Selecao('Panamá', False)
 tunisia = Selecao('Tunísia', False)
 
-grupoG = [belgica, inglaterra, panama, tunisia]
+grupoG = [belgica, inglaterra, panama, tunisia]  # Objetos (seleções) do Grupo G guardados na lista grupoG
 
 # Grupo H
 colombia = Selecao('Colômbia', False)
@@ -130,11 +151,41 @@ japao = Selecao('Japão', False)
 polonia = Selecao('Polônia', False)
 senegal = Selecao('Senegal', False)
 
-grupoH = [colombia, japao, polonia, senegal]
+grupoH = [colombia, japao, polonia, senegal]  # Objetos (seleções) do Grupo H guardados na lista grupoH
 
-for selecao in grupoA:
-    print(selecao.nome)
-    print(selecao.status)
+"""Organizando confronto de seleções para que este nao se repita
+Método combinations() transforma os objetos em cada grupo em tuplas com confrontos únicos"""
+# As tentativas de automatizar o combinations() com um laço for e a lista geral copaMundo não surtiram efeito.
+print(grupoA)
+grupoA = list(combinations(grupoA, 2))
+grupoB = list(combinations(grupoB, 2))
+grupoC = list(combinations(grupoC, 2))
+grupoD = list(combinations(grupoD, 2))
+grupoE = list(combinations(grupoE, 2))
+grupoF = list(combinations(grupoF, 2))
+grupoG = list(combinations(grupoG, 2))
+grupoH = list(combinations(grupoH, 2))
+
+"""Organização geral dos grupos"""
+copaMundo = [grupoA, grupoB, grupoC, grupoD, grupoE, grupoF, grupoG, grupoH]
+
+print(copaMundo[0])
+
+ordem = [(1, 0, 5), (2, 1, 4), (3, 2, 3)]
+
+print(len(copaMundo[0]))
+
+for rodada in ordem:
+    cabecalhoRodada(rodada[0])
+    for grupo in range(2):
+        cabecalhoGrupo(nomesGrupos[grupo])
+        Selecao.partida(Selecao, copaMundo[grupo][rodada[1]][0], copaMundo[grupo][rodada[1]][1])
+        Selecao.partida(Selecao, copaMundo[grupo][rodada[2]][0], copaMundo[grupo][rodada[2]][1])
+
+
+
+
+
 
 # for brasil. partida = lista com adversarios para ir percorrendo
 # Cadastrar escalação de cada seleção e colocar quem fez o gol em cada partida e em qual minuto.
